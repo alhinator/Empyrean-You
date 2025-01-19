c569cf95d32135354aee2e231f210f273a25598b
alhinator <alex@leghart.org>
2025-01-19 11:05:40 -0800

    Added font assets, Updates to player movment

---
+++ Assets/HUDManager.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HUDManager : MonoBehaviour
+{
+    public Player3PCam player3PCam;
+    public Canvas staticPlayerHud;
+    public TMP_Text BoostBars;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this needs to be converted to an event-based system to avoid hud updates every frame.
+        UpdateBasicHudText();
+    }
+
+    private void UpdateBasicHudText()
+    {
+        //Boost bar squares
+        string tmp = "";
+        for (int i = 0; i < player3PCam.BoostsRemaining; i++) { tmp += "■"; }
+        BoostBars.text = tmp;
+    }
+}
--- Assets/Scripts/Player/Player3PCam.cs
+++ Assets/Scripts/Player/Player3PCam.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Android;
 
 public class Player3PCam : MonoBehaviour
 {
@@ -19,27 +20,30 @@ public class Player3PCam : MonoBehaviour
     public float playerHeight;
     public LayerMask terrainMask;
     public float groundDrag;
-    private bool isGrounded;
     public float jumpForce;
     public float airMultiplier;
     public float hoverMultiplier;
-
     public float jumpCooldown;
-    private float canJump;
     public float doubleTapDelay;
-    private float dashTapCount;
     public float dashForce;
-    public float maxMidairBoosts;
-    private float currMidairBoosts;
-    private bool hovering;
+    public int maxMidairBoosts;
 
+    private int currMidairBoosts;
+    private bool hovering;
+    private bool isGrounded;
+    private float canJump;
+    private float dashTapCount;
+    private float boostTapCount;
+    private float sprintDuration;
+
+
+    //Unity Functions
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         rb = player.GetComponent<Rigidbody>();
         rb.freezeRotation = true;
-
     }
     private void Update()
     {
@@ -56,13 +60,12 @@ public class Player3PCam : MonoBehaviour
             rb.velocity = Vector3.zero;
         }
     }
-
     private void FixedUpdate()
     {
         MovePlayer();
     }
 
-
+    //Private Functions
     private void Do3PCameraMovement()
     {
         //Code adapted from https://www.youtube.com/watch?v=UCwwn2q4Vys
@@ -73,12 +76,21 @@ public class Player3PCam : MonoBehaviour
         Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
 
-        if (horizontalInput != 0 || verticalInput != 0)
+        if ((horizontalInput != 0 || verticalInput != 0) || !isGrounded)
         {
             Vector3 offset = new Vector3(orientation.forward.x, orientation.forward.y, orientation.forward.z);
-            if (Input.GetAxis("SPRINT") > 0 && verticalInput > 0)
+            if (Input.GetAxis("SPRINT") > 0 && sprintDuration > doubleTapDelay)
             {
-                offset = Quaternion.AngleAxis(45 * horizontalInput, Vector3.up) * offset;
+
+                bool a, b, c, d, e, f;
+                a = verticalInput == 0;
+                b = horizontalInput == 0;
+                c = verticalInput == 1;
+                d = horizontalInput == 1;
+                e = verticalInput == -1;
+                f = horizontalInput == -1;
+                float offAngle = (a && b) || (b && c) ? 0 : a && d ? 90 : a && f ? -90 : b && e ? 180 : c && d ? 45 : c && f ? -45 : e && f ? -135 : d && e ? 135 : 0;
+                offset = Quaternion.AngleAxis(offAngle, Vector3.up) * offset;
             }
             playerObj.forward = Vector3.Slerp(playerObj.forward, offset, Time.deltaTime * rotationSpeed);
 
@@ -96,13 +108,19 @@ public class Player3PCam : MonoBehaviour
 
         if (isGrounded)
         {
-            if (sprinting > 0 && verticalInput > 0)
+            if (sprinting > 0)
+            {
+                sprintDuration += Time.deltaTime;
+
+            }
+            else if (sprinting > 0 && sprintDuration > doubleTapDelay && (horizontalInput != 0 || verticalInput != 0))
             {
                 playerObj.GetComponentInChildren<Animator>().SetBool("Sprinting", true);
                 rb.AddForce(moveDirection.normalized * sprintSpeed, ForceMode.Force);
             }
             else
             {
+                sprintDuration = 0;
                 playerObj.GetComponentInChildren<Animator>().SetBool("Sprinting", false);
                 rb.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Force);
             }
@@ -151,38 +169,49 @@ public class Player3PCam : MonoBehaviour
         }
         else if (Input.GetButtonDown("SPACE") && !isGrounded)
         {
+            DoMidairBoostCheck();
+        }
+
+    }
+    private void DoMidairBoostCheck()
+    {
+        if (boostTapCount == 0)
+        {
+            Debug.Log("boost Taps = 1");
+            boostTapCount = 1;
             StartCoroutine(BoostOrHover());
         }
-        else if (Input.GetButtonUp("SPACE") && !isGrounded)
+        else if (boostTapCount == 1)
         {
-            hovering = false;
+            Debug.Log("Taps = 2");
+            boostTapCount = 2;
+            StartCoroutine(BoostOrHover());
         }
     }
     private IEnumerator BoostOrHover()
     {
         yield return new WaitForSeconds(doubleTapDelay);
-        //if button is up, that means they've released and want to boost upwards. if it's still down they want to hover indefinitely.
-        if (Input.GetAxis("SPACE") == 0 && currMidairBoosts > 0)
-        {
+        if (boostTapCount == 1 && currMidairBoosts > 0)
+        { //means they want to boost upwards
+            hovering = false;
             currMidairBoosts--;
             rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-            rb.AddForce(player.transform.up * jumpForce * 1.5f, ForceMode.Impulse);
+            rb.AddForce(1.5f * jumpForce * player.transform.up, ForceMode.Impulse);
         }
-        else
+        else if (boostTapCount == 2 || (boostTapCount == 1 && currMidairBoosts <= 0))
         {
-            hovering = true;
+            StopCoroutine(BoostOrHover());
+            hovering = !hovering;
         }
+        boostTapCount = 0;
 
     }
-
-
     private IEnumerator DoActualJump()
     {
         yield return new WaitForSeconds(0.2f);
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
         rb.AddForce(player.transform.up * jumpForce, ForceMode.Impulse);
     }
-
     private void DoDashCheck()
     {
         //Code modified from https://discussions.unity.com/t/single-tap-double-tap-script/440934/5
@@ -202,7 +231,6 @@ public class Player3PCam : MonoBehaviour
             }
         }
     }
-
     private IEnumerator DashTap()
     {
         yield return new WaitForSeconds(doubleTapDelay);
@@ -214,6 +242,7 @@ public class Player3PCam : MonoBehaviour
             else
             {
                 //do dash stuff here.
+                rb.velocity = new Vector3(0, rb.velocity.y, 0);
                 rb.AddForce(playerObj.transform.forward * -1 * dashForce / 2, ForceMode.Impulse);
             }
         }
@@ -226,15 +255,20 @@ public class Player3PCam : MonoBehaviour
             }
             else
             {
+                Vector3 dashDir = (playerObj.transform.right * Input.GetAxis("Horizontal") + playerObj.transform.forward * Input.GetAxis("Vertical")).normalized * dashForce;
+                if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+                {
+                    dashDir = (playerObj.transform.forward * -1).normalized * dashForce;
+                }
                 //do extended dash stuff here.
                 if (isGrounded)
                 {
-                    rb.AddForce((playerObj.transform.right * Input.GetAxis("Horizontal") + playerObj.transform.forward * Input.GetAxis("Vertical")).normalized * dashForce, ForceMode.Impulse);
+                    rb.AddForce(dashDir, ForceMode.Impulse);
                 }
                 else if (!isGrounded && currMidairBoosts > 0)
                 {
                     currMidairBoosts--;
-                    rb.AddForce((playerObj.transform.right * Input.GetAxis("Horizontal") + playerObj.transform.forward * Input.GetAxis("Vertical")).normalized * dashForce, ForceMode.Impulse);
+                    rb.AddForce(dashDir, ForceMode.Impulse);
                 }
             }
 
@@ -242,6 +276,15 @@ public class Player3PCam : MonoBehaviour
         dashTapCount = 0;
 
 
+    }
+
+    //  Public Getters & Setters
+    public int BoostsRemaining
+    {
+        get
+        {
+            return currMidairBoosts;
+        }
     }
 
 }
