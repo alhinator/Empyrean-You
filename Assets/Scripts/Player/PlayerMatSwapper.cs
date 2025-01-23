d77774a6f3443d44ac5d9e256d7490b4c5383452
alhinator <alex@leghart.org>
2025-01-23 11:00:56 -0800

    Working on start screen UI

--- Assets/Scripts/Player/PlayerMatSwapper.cs
+++ Assets/Scripts/Player/PlayerMatSwapper.cs
@@ -1,49 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEditor.ShortcutManagement;
 using UnityEngine;
 
 public class PlayerMatSwapper : MonoBehaviour
 {
-    public Material basePlayerMaterial;
+    public Material baseMaterial;
+    public Material variableMaterial;
     public Material invisibleMaterial;
     public Material coreMaterial;
 
+    public Shader WireframeOpaque;
+    public Shader WireframeTrans;
 
-    private readonly MeshRenderer[] playerMeshes;
-    public MeshRenderer chestLightUpMesh;
+    public bool DebugInvis;
+
     // Start is called before the first frame update
     void Start()
     {
-        var tmpObjs = GameObject.FindGameObjectsWithTag("PlayerMesh");
-        foreach (var obj in tmpObjs)
-        {
-            TryGetComponent<MeshRenderer>(out MeshRenderer m);
-            if (m != null)
-            {
-                playerMeshes.Append(m);
-            }
-        }
+        variableMaterial.shader = WireframeOpaque;
+        SetPlayerColor(Color.black, Color.black);
+        DebugInvis = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    }
-
-    void SetPlayerColor(Color line, Color fill)
-    {
-
-    }
-    void StartLerpPlayerColor(Color line, Color fill)
-    {
-
-    }
-    void ResetPlayerColor()
-    {
-        foreach(MeshRenderer m in playerMeshes){
-            m.material = basePlayerMaterial;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DebugInvis = !DebugInvis;
+            SetInvis(DebugInvis);
         }
+    }
+
+    public void SetPlayerColor(Color line, Color fill)
+    {
+        variableMaterial.SetColor(Shader.PropertyToID("_WireColor"), line);
+        variableMaterial.SetColor(Shader.PropertyToID("_BaseColor"), fill);
+    }
+    public void SetInvis(bool enabled)
+    {
+        if (enabled)
+        {
+            variableMaterial.shader = WireframeTrans;
+            coreMaterial.shader = WireframeTrans;
+            StartCoroutine(LerpFade(variableMaterial, baseMaterial, invisibleMaterial));
+        }
+        else
+        {
+            variableMaterial.shader = WireframeOpaque;
+            coreMaterial.shader = WireframeOpaque;
+            StartCoroutine(LerpFade(variableMaterial, invisibleMaterial, baseMaterial));
+
+        }
+    }
+    IEnumerator LerpFade(Material active, Material previous, Material next)
+    {
+        active.CopyMatchingPropertiesFromMaterial(previous);
+        for (int i = 0; i <= 100; i++)
+        {
+            active.Lerp(previous, next, i * 0.01f);
+            yield return new WaitForSeconds(0.005f);
+        }
+    }
+
+    void OnDestroy()
+    {
+        SetPlayerColor(Color.black, Color.black);
+        variableMaterial.shader = WireframeOpaque;
+        coreMaterial.shader = WireframeOpaque;
     }
 }
