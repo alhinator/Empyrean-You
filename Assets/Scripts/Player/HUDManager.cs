c1f5fbcd149f37addb3f4c55208de3f661a083a9
alhinator <alex@leghart.org>
2025-01-25 13:33:49 -0800

    Reticle mostly working.

--- Assets/Scripts/Player/HUDManager.cs
+++ Assets/Scripts/Player/HUDManager.cs
@@ -1,15 +1,21 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUDManager : MonoBehaviour
 {
     public Player3PCam player3PCam;
+    public Camera mainCamera;
     public Canvas staticPlayerHud;
     public TMP_Text BoostBars;
     // Start is called before the first frame update
+
+
+    [Header("Reticle Variables")]
+    public float reticleSpeed;
+    public Image reticle;
+    Vector2 reticleIdealPosition;
+
     void Start()
     {
 
@@ -20,6 +26,9 @@ public class HUDManager : MonoBehaviour
     {
         //this needs to be converted to an event-based system to avoid hud updates every frame.
         UpdateBasicHudText();
+        SetReticleIdealPosition();
+        MoveReticle();
+
     }
 
     private void UpdateBasicHudText()
@@ -28,5 +37,17 @@ public class HUDManager : MonoBehaviour
         string tmp = "";
         for (int i = 0; i < player3PCam.BoostsRemaining; i++) { tmp += "*"; }
         BoostBars.text = tmp;
+    }
+    private void SetReticleIdealPosition(){
+        if(player3PCam.currentTargetLock){
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(staticPlayerHud.GetComponent<RectTransform>(), mainCamera.WorldToScreenPoint(player3PCam.currentTargetLock.position), mainCamera, out Vector2 temp);
+            reticleIdealPosition = new Vector3(temp.x, temp.y, 0);
+        }else
+        {
+            reticleIdealPosition = new Vector3(0, 0, 0);
+        }
+    }
+    private void MoveReticle(){
+        reticle.rectTransform.localPosition = Vector3.Lerp(reticle.transform.localPosition, reticleIdealPosition, reticleSpeed * Time.deltaTime);
     }
 }
