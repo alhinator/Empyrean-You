9ebe96f1169d6b6376cf9fc78c46034b5ac98bc7
alhinator <alex@leghart.org>
2025-02-08 16:15:00 -0800

    Working on title screen & title screen localization

--- Assets/Scripts/UI/MainMenuScript.cs
+++ Assets/Scripts/UI/MainMenuScript.cs
@@ -1,38 +1,108 @@
+using System;
 using System.Collections;
+using TMPro;
+using UnityEditor;
+using UnityEditor.Localization.Editor;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.SceneManagement;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+using UnityEngine.Localization.Tables;
+using UnityEngine.SocialPlatforms;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public enum STATE { BOOTUP, ELEVATOR, COLORSELECT, POWERSELECT, GUNSELECT, NAMESELECT };
+    private static MainMenuScript Singleton;
     [Header("Canvases")]
-    public Canvas mainMenuCanvas;
     public Canvas worldCanvas;
+    public Canvas mainMenuCanvas;
     public Canvas leftScreen;
     public Canvas rightScreen;
     public Canvas remappingCanvas;
 
-    [Header("Others")]
+    [Header("Objects")]
     public Transform elevator;
     public EventSystem eventSystem;
+
+    [Header("State")]
+    private static STATE currState;
+
+    [Header("Main Menu Variables")]
+
+
+    [Header("Color Select Variables")]
     public GameObject selectMeAfterElevator;
-    // Start is called before the first frame update
+    public TMP_Text LeftScreenHeader;
+
+    [Header("Localization")]
+    private StringTable mmStrings;
+    public TMP_Text subtitle;
+
     void Start()
     {
-        worldCanvas.enabled = false;
-        mainMenuCanvas.enabled = true;
-        leftScreen.enabled = false;
-        rightScreen.enabled = false;
-        remappingCanvas.enabled = false;
+        if (Singleton == null)
+        {
+            Singleton = this;
+        }
+        else
+        {
+            Destroy(this);
+            return;
+        }
+        mmStrings = LocalizationSettings.StringDatabase.GetTable("Title Screen", null);
+        Singleton.TransitionToState(STATE.BOOTUP);
+    }
+
+    /// <summary>
+    /// The manager for the FFSM: Fake Finite State Machine. Unfortunately very smelly but I don't have the leisure to implement a better option atm.
+    /// </summary>
+    /// <param name="newState">The new state to transition to.</param>
+    public void TransitionToState(STATE newState)
+    {
+        //First, anything that needs to be done when leaving the previous state?
+        switch (currState)
+        {
+            case STATE.BOOTUP:
+                break;
+        }
+        //now, anything that happens when entering the next state:
+        currState = newState;
+        switch (currState)
+        {
+            case STATE.BOOTUP:
+                Singleton.subtitle.enabled = false;
+                Singleton.worldCanvas.enabled = false;
+                Singleton.mainMenuCanvas.enabled = true;
+                Singleton.leftScreen.enabled = false;
+                Singleton.rightScreen.enabled = false;
+                Singleton.remappingCanvas.enabled = false;
+                break;
+            case STATE.ELEVATOR:
+                Singleton.remappingCanvas.enabled = false;
+                Singleton.mainMenuCanvas.enabled = false;
+                Singleton.eventSystem.SetSelectedGameObject(null);
+                Singleton.StartCoroutine(DescendToHangar());
+                break;
+            case STATE.COLORSELECT:
+                Singleton.mainMenuCanvas.enabled = false;
+                Singleton.remappingCanvas.enabled = false;
+                Singleton.worldCanvas.enabled = true;
+                Singleton.worldCanvas.sortingOrder = 2;
+                Singleton.leftScreen.enabled = true;
+                Singleton.eventSystem.SetSelectedGameObject(Singleton.selectMeAfterElevator);
+
+                ///play a voiceline here / display text
+                Singleton.subtitle.enabled = true;
+                Singleton.subtitle.text = mmStrings.GetEntry("title_screen.subtitle.empty").Value;
+
+
+                break;
+        }
     }
     public void StartDescent()
     {
-        StartCoroutine(DescendToHangar());
-        remappingCanvas.enabled = false;
-        mainMenuCanvas.enabled = false;
-        eventSystem.SetSelectedGameObject(null);
-        //SceneManager.LoadScene("GrayboxMap");
-
+        TransitionToState(STATE.ELEVATOR);
     }
     private IEnumerator DescendToHangar()
     {
@@ -40,19 +110,16 @@ public class MainMenuScript : MonoBehaviour
         for (float percent = 0; percent <= 1000; percent++)
         {
 
-            elevator.position = new Vector3(elevator.position.x, Mathf.Lerp(300, 1, percent / 1000), elevator.position.z);
+            Singleton.elevator.position = new Vector3(Singleton.elevator.position.x, Mathf.Lerp(300, 1, percent / 1000), Singleton.elevator.position.z);
             yield return new WaitForSeconds(0.01f);
         }
-        worldCanvas.enabled = true;
-        worldCanvas.sortingOrder = 2;
-        leftScreen.enabled = true;
-        eventSystem.SetSelectedGameObject(selectMeAfterElevator);
+        Singleton.TransitionToState(STATE.COLORSELECT);
 
     }
 
     public void ToggleRemappingCanvas()
     {
-        remappingCanvas.enabled = !remappingCanvas.enabled;
+        Singleton.remappingCanvas.enabled = !Singleton.remappingCanvas.enabled;
     }
 
 
